8e852251f4a6168fd32d133a724de595d1cb83c2
root <root@vmcentos1.(none)>
2013-10-19 20:31:51 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App::Application.config.secret_key_base = 'dafb3331c7971f951ab5835b92f63f95cc6a5a0e7b66b54fdbdf65fec2142fa80beb7b3bc55ec20792f182731250f5b3bbd1be215d4464b06de78ca576b8f8c4'
